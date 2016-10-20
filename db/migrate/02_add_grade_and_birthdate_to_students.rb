c6a0ca03811a2ab7d9638ed4292ac18f4333e9ac
Keo  Bun <keonilrath@gmail.com>
2016-10-20 02:02:34 +0000

    Done.

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,2 +1,7 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration
+  def change
+    add_column :students, :grade, :integer
+    add_column :students, :birthdate, :string
+  end
+
 end
--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,2 +1,5 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration
+  def change
+    change_column(:students, :birthdate, :datetime)
+  end
 end
